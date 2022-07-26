cdab90a300779e83e8bb872e02cc22ec0bf03f7e
Marius Matei <mariusmatei31@gmail.com>
2022-07-26 17:50:16 +0300

    move phpactor keymaps to whichkey

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -37,27 +37,27 @@ vim.cmd [[
 
   augroup _php
     autocmd!
-    autocmd FileType php setlocal omnifunc=phpactor#Complete
-    au FileType php nmap <buffer> <Leader>.eu :PhpactorImportClass<CR>
-    au FileType php nmap <buffer> <Leader>.ce :PhpactorClassExpand<CR>
-    au FileType php vnoremap <buffer> <Leader>.em :PhpactorExtractMethod<CR>
-    au FileType php nnoremap <buffer> <Leader>.ec :PhpactorExtractConstant<CR>
-    au FileType php nmap <buffer> <Leader>.ic :PhpactorImportMissingClasses<CR>
-    au FileType php nmap <buffer> <Leader>. :PhpactorContextMenu<CR>
-    au FileType php nmap <buffer> <Leader>.nn :PhpactorNavigate<CR>
-    au FileType php,cucumber nmap <buffer> <Leader>.gd
-        \ :PhpactorGotoDefinition edit<CR>
-    au FileType php nmap <buffer> <Leader>.k :PhpactorHover<CR>
-    au FileType php nmap <buffer> <Leader>.cc :PhpactorClassNew<CR>
-    au FileType php nmap <buffer> <Leader>.ci :PhpactorClassInflect<CR>
-    au FileType php nmap <buffer> <Leader>.fr :PhpactorFindReferences<CR>
-    au FileType php nmap <buffer> <Leader>.ep :PhpactorTransform<CR>
-    au FileType php nmap <buffer> <Leader>.mf :PhpactorMoveFile<CR>
-    au FileType php nmap <buffer> <Leader>.cf :PhpactorCopyFile<CR>
-    au FileType php nmap <buffer> <silent> <Leader>.ee
-        \ :PhpactorExtractExpression<CR>
-    au FileType php vmap <buffer> <silent> <Leader>.ee
-        \ :<C-u>PhpactorExtractExpression<CR>
+    au FileType php setlocal omnifunc=phpactor#Complete
+    "au FileType php nmap <buffer> <Leader>,eu :PhpactorImportClass<CR>
+    "au FileType php nmap <buffer> <Leader>,ce :PhpactorClassExpand<CR>
+    "au FileType php vnoremap <buffer> <Leader>,em :PhpactorExtractMethod<CR>
+    "au FileType php nnoremap <buffer> <Leader>,ec :PhpactorExtractConstant<CR>
+    "au FileType php nmap <buffer> <Leader>,ic :PhpactorImportMissingClasses<CR>
+    "au FileType php nmap <buffer> <Leader>. :PhpactorContextMenu<CR>
+    "au FileType php nmap <buffer> <Leader>,nn :PhpactorNavigate<CR>
+    "au FileType php,cucumber nmap <buffer> <Leader>.gd
+        "\ :PhpactorGotoDefinition edit<CR>
+    "au FileType php nmap <buffer> <Leader>,k :PhpactorHover<CR>
+    "au FileType php nmap <buffer> <Leader>,cc :PhpactorClassNew<CR>
+    "au FileType php nmap <buffer> <Leader>,ci :PhpactorClassInflect<CR>
+    "au FileType php nmap <buffer> <Leader>,fr :PhpactorFindReferences<CR>
+    "au FileType php nmap <buffer> <Leader>,ep :PhpactorTransform<CR>
+    "au FileType php nmap <buffer> <Leader>,mf :PhpactorMoveFile<CR>
+    "au FileType php nmap <buffer> <Leader>,cf :PhpactorCopyFile<CR>
+    "au FileType php nmap <buffer> <silent> <Leader>,ee
+        "\ :PhpactorExtractExpression<CR>
+    "au FileType php vmap <buffer> <silent> <Leader>,ee
+        "\ :<C-u>PhpactorExtractExpression<CR>
   augroup end
 
   augroup resCur
